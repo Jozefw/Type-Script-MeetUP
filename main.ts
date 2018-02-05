3f5236cbceafc45df467143ed8790e76ce496307
Jozef <jozefw@sbcglobal.net>
2018-02-05 08:00:06 -0800

    data types etc

--- main.ts
+++ main.ts
@@ -1,6 +1,29 @@
-function logger(message){
-    console.log(message);
-}
+// ************ Type Annotations ************
 
-let message = "Hey there";
-logger(message);
+let a: number;
+let b: boolean;
+let c: string;
+let d: any;
+let e: number[]=[1,2,3];
+let f: any[]=[1,true,'a',false]
+
+// *********** ENUM ***********
+const ColorRed = 0;
+const ColorBlue = 1;
+const ColorGreen = 2;
+
+enum color {Red = 0,Green = 1,Blue = 2};
+let backgroundColor = Color.Red;
+
+// *********** TYPE ASSERTIONS ***********
+
+// message is type 'any'
+let message;
+// may throw error 
+let message = 'abc';
+assert type <string> onto message
+let endswithc = (<string>message).endsWith('c');
+let endswithc = (message as string).endsWith('c');
+
+// ************** Interfaces ***********
+
